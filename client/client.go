225cc22c995e6fabbf49e1ac9d88dec39c3d2291
CosmicSparX <kpranay661@gmail.com>
2024-07-29 21:51:49 +0530

    Working on the handshake part of the tcp connection

---
+++ client/client.go
@@ -0,0 +1,20 @@
+package client
+
+import (
+	"github.com/CosmicSparX/bitTorrent-Clinet/peers"
+	"net"
+	"time"
+)
+
+type Client struct {
+	Conn   net.Conn
+	Choked bool
+}
+
+func New(peer peers.Peer, peerID string, infoHash [20]byte) (*Client, error) {
+	conn, err := net.DialTimeout("tcp", peer.String(), 3*time.Second)
+	if err != nil {
+		return nil, err
+	}
+
+}
