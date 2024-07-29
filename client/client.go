f9ecd630574e568b7165eddaf800abd5ebef623d
CosmicSparX <kpranay661@gmail.com>
2024-07-30 00:18:55 +0530

    Added Message parsing to the tcp connection

--- client/client.go
+++ client/client.go
@@ -1,14 +1,63 @@
 package client
 
 import (
+	"bytes"
+	"fmt"
+	"github.com/CosmicSparX/bitTorrent-Clinet/bitfield"
+	"github.com/CosmicSparX/bitTorrent-Clinet/handshake"
+	"github.com/CosmicSparX/bitTorrent-Clinet/message"
 	"github.com/CosmicSparX/bitTorrent-Clinet/peers"
 	"net"
 	"time"
 )
 
 type Client struct {
-	Conn   net.Conn
-	Choked bool
+	Conn     net.Conn
+	Choked   bool
+	Bitfield bitfield.BitField
+	peer     peers.Peer
+	infoHash [20]byte
+	peerID   [20]byte
+}
+
+func completeHandshake(conn net.Conn, infoHash, peerID [20]byte) (*handshake.Handshake, error) {
+	conn.SetDeadline(time.Now().Add(3 * time.Second))
+	defer conn.SetDeadline(time.Time{}) // disable the deadline
+
+	req := handshake.New(infoHash, peerID)
+	_, err := conn.Write(req.Serialize())
+	if err != nil {
+		return nil, err
+	}
+
+	res, err := handshake.Read(conn)
+	if err != nil {
+		return nil, err
+	}
+
+	if !bytes.Equal(res.InfoHash[:], infoHash[:]) {
+		err := fmt.Errorf("Expected InfoHash %x but got %x", infoHash, res.InfoHash)
+		return nil, err
+	}
+
+	return res, nil
+}
+
+func recvBitField(conn net.Conn) (bitfield.BitField, error) {
+	conn.SetDeadline(time.Now().Add(5 * time.Second))
+	defer conn.SetDeadline(time.Time{}) // Disable the Deadline
+
+	msg, err := message.Read(conn)
+	if err != nil {
+		return nil, err
+	}
+
+	if msg.ID != message.MsgBitfield {
+		err := fmt.Errorf("expected bitfield but got ID %d", msg.ID)
+		return nil, err
+	}
+
+	return msg.Payload, nil
 }
 
 func New(peer peers.Peer, peerID, infoHash [20]byte) (*Client, error) {
@@ -17,4 +66,22 @@ func New(peer peers.Peer, peerID, infoHash [20]byte) (*Client, error) {
 		return nil, err
 	}
 
+	_, err = completeHandshake(conn, infoHash, peerID)
+	if err != nil {
+		return nil, err
+	}
+
+	bf, err := recvBitField(conn)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Client{
+		Conn:     conn,
+		Choked:   true,
+		Bitfield: bf,
+		peer:     peer,
+		infoHash: infoHash,
+		peerID:   peerID,
+	}, nil
 }
